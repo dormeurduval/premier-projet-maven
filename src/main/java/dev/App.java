498f7d76d02a2fae3e48b63988403360cff6fdd0
CHAFFARD Joris <chaffard.joris@outlook.fr>
2017-09-28 12:40:31 +0200

    second commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,12 @@
 package dev;
 
-/**
- * Hello world!
- *
- */
+import com.github.lalyos.jfiglet.FigletFont;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String asciiArt = FigletFont.convertOneLine("hello");
+        System.out.println(asciiArt);
     }
 }
